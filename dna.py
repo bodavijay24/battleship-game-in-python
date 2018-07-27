aade46ce6407d26b8716c085d30492c269294ffe
Vijay <vijaykumar.b15@iiits.in>
2018-07-27 10:54:21 +0530

    Create dna.py

---
+++ dna.py
@@ -0,0 +1,55 @@
+"""The project should have methods for each of the following:
+
+Given a file, read in the DNA for each suspect and save it as a string
+Take a DNA string and split it into a list of codons
+Iterate through a suspect's codon list to see how many of their codons match the sample codons
+Pick the right suspect to continue the investigation on
+
+@Bodavijay
+"""
+sample = ['GTA','GGG','CAC']
+
+def read_dna(dna_file):
+  dna_data=""
+  with open(dna_file,"r") as f:
+    for line in f:
+      dna_data+=line
+      
+  return dna_data
+def dna_codons(dna):
+  codons=[]
+  for i in range(0,len(dna),3):
+    if (i+3) <len(dna):
+      codons.append(dna[i:i+3])
+      
+  return codons
+
+def match_dna(dna):
+  matches=0
+  
+  for i in dna:
+    if i in sample:
+      matches+=1
+  return matches
+
+def is_criminal(dna_sample):
+  dna_data=read_dna(dna_sample)
+  codons=dna_codons(dna_data)
+  num_matches=match_dna(codons)
+  if num_matches >=3:
+    print "Num of matches %d ,  investigation should continue "%(num_matches)
+  else:
+    print "Stating the suspect can be freed."
+
+is_criminal('suspect1.txt')
+is_criminal('suspect2.txt')
+is_criminal('suspect3.txt')
+
+  
+      
+  
+  
+
+      
+    
+  
